58763d8f2e65ced29ad63fbd1639886613f352a5
DigitalizacionAgroinge <digitalizacion@agroinge.com>
2022-03-04 11:08:20 +0100

    v1_webservice

---
+++ ws-Agroinge-simulador/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ws_Agroinge_simulador.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
